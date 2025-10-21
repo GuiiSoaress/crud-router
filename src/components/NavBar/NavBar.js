88b0a5082d55a0388ecc4d17299e20ac6ad76e5c
Guilherme Soares <guihhsoaress@gmail.com>
2025-10-20 21:05:03 -0300

    NEW: Page Cadastro.js

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,20 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
-//import "./NavBar.css";
+import "./NavBar.css";
 
-function NavBar({onLogout}){
-
-    return (
-        <nav className="nav">
-            <h2>Agenda </h2>
-            <ul>
-                <li>Home</li>
-                <li>Cadastrar</li>
-                <li>Listar Contatos</li>
-                <li>Sair</li>
-            </ul>
-        </nav>
-    )
+function NavBar({ onLogout }) {
+  return (
+    <nav className="navbar">
+      <h2>Agenda</h2>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/cadastro">Cadastrar</Link>
+        </li>
+        <li>
+          <Link to="/lista">Listar Contatos</Link>
+        </li>
+        <li>
+          <button onClick={onLogout}>Sair</button>
+        </li>
+      </ul>
+    </nav>
+  );
 }
-
 export default NavBar;
